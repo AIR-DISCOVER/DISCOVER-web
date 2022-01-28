25d5135a97f17d866c9675933c5e92fd9d74529b
TB5zhh <tianbeiwen88@gmail.com>
2022-01-28 21:54:52 +0800

    add rotation of camera

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
-import React, { Suspense} from 'react'
-import { Canvas, useLoader } from '@react-three/fiber'
-
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense, useRef } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrthographicCamera } from 'three';
-import { PerspectiveCamera } from 'three';
-import { useSpring, animated, config } from 'react-spring';
 const Model = () => {
   const ref = useRef()
   const gltf = useLoader(GLTFLoader, "./rest.gltf");
@@ -18,27 +13,11 @@ const Model = () => {
   );
 };
 
-const Controls = () => {
-  const { gl, camera } = useThree()
-  camera.setFocalLength(10)
-  useSpring({
-    loop: true,
-    from: {
-      z: 300
-    },
-    to: {
-      z: 300,
-    },
-    // onFrame: ({ z }) => {
-    //   camera.position.z = z
-    // }
-  })
-  return <OrbitControls target={[0, 0, 0]} args={[camera, gl.domElement]} />
-}
 function Dolly() {
   // This one makes the camera move in and out
   useFrame(({ clock, camera }) => {
-    camera.position.z = 50 + Math.sin(clock.getElapsedTime()) * 30
+    camera.position.z = 0 + Math.sin(clock.getElapsedTime()) * 30
+    camera.rotation.y = Math.PI / 2 * Math.cos(clock.getElapsedTime() * 0.5)
   })
   return null
 }
