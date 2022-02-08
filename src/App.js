0b2c21046303dc00b8922e27fe1af244e991db62
zhaochen20 <zhaochenyang20@gmail.com>
2022-02-08 20:48:35 +0800

    function to return each point along each way

--- src/App.js
+++ src/App.js
@@ -131,6 +131,137 @@ const Scene = (state) => {
   );
 }
 
+const fn = function (x, y, z)
+{
+  if (x === "News" && y === "About")
+  {
+    if( z <= 0.4) { let tmp = z;  camera.position.set(-30 + 30 * tmp, 0, 100 - 200 * tmp); camera.rotation.set(0,0,0);}
+    else if( z <= 0.5) {let tmp = z - 0.4;  camera.position.set(-18 + 10 * tmp, 0, 20 - 200 * tmp);  camera.rotation.set(0,0,0);}
+    else if(z <= 0.6) {let tmp = z - 0.5; camera.position.set(-17, 0, -200 * tmp); camera.rotation.set(0, 0, 0);}
+    else if( z <= 0.7) {let tmp = z - 0.6; camera.position.set(-17 - 30 * tmp, 0, -20); camera.rotation.set(0, - Math.PI * tmp / 2.4, 0);}
+    else if( z <= 1 ) {let tmp = z - 0.7; camera.position.set( -20 - 200 * tmp, 0, -20); camera.rotation.set(0, - Math.PI / 24 - Math.PI * tmp / 2.4, 0);}
+  }
+  else if (x === "About" && y === "News")
+  {
+    if(z <= 0.3) {let tmp = z;  camera.position.set(-80 + 200 * tmp, 0, -20); camera.rotation.set(0, - Math.PI / 6 + Math.PI * tmp / 2.4, 0);}
+    else if( z <= 0.4) { let tmp = z - 0.3; camera.position.set(-20 + 30 * tmp, 0, -20); camera.rotation.set(0, - Math.PI / 24 + Math.PI * tmp / 2.4, 0);}
+    else if( z <= 0.5) { let tmp = z - 0.4; camera.position.set(-17, 0, -20 + 200 * tmp); camera.rotation.set(0, 0, 0);}
+    else if( z <= 0.6) { let tmp = z- 0.5; camera.position.set(-17 - 10 * tmp, 0, 200 * tmp); camera.rotation.set(0, 0, 0);}
+    else if( z <= 1) { let tmp = z - 0.6; camera.position.set(-18 - 30 * tmp, 0, 20 + 200 * tmp); camera.rotation.set(0, 0, 0);}
+  }
+  else if(x === "About" && y === "Research")
+  {
+    if( z <= 0.3) {let tmp = z; camera.position.set(-80 + 200 * tmp, 0, -20); camera.rotation.set(0, -Math.PI / 6 + Math.PI * tmp / 2.4, 0);}
+    else if( z <= 0.4) {let tmp = z - 0.3; camera.position.set(-20 + 30 * tmp, 0, -20); camera.rotation.set(0, - Math.PI / 24 + Math.PI * tmp / 2.4, 0);}
+    else if( z <= 0.5) {let tmp = z - 0.4; camera.position.set(-17, -20 + 2200 * tmp, 0); camera.rotation.set(0, 0, 0);}
+    else if( z <= 0.6) {let tmp = z - 0.5; camera.position.set(-17 - 30 * tmp, 200 + 100 * tmp, 0); camera.rotation.set(0, 0, 0);}
+    else if( z <= 0.7) {let tmp = z - 0.6; camera.position.set(-20 - 150 * tmp, 0, 210); camera.rotation.set(0, Math.PI * tmp / 1.8, 0);}
+    else if( z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-35 - 10 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 18 + Math.PI * tmp / 1.8, 0);}
+  }
+  else if(x === "Research" && y === "About")
+  {
+    if(z <= 0.3) {let tmp = z / 3; camera.position.set(-45 + 10 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 9 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 0.4) {let tmp = z - 0.3; camera.position.set(-35 + 150 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 18 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 0.5) {let tmp = z - 0.4; camera.position.set(-20 + 30 * tmp, 210 - 100 * tmp, 0); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.6) {let tmp = z - 0.5; camera.position.set(-17, 200 - 2200 * tmp, 0); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.7) {let tmp = z - 0.6; camera.position.set(-17 - 30 * tmp, 0, -20); camera.rotation.set(0, - Math.PI * tmp / 2.4, 0);}
+    else if(z <= 1) {let tmp = z - 0.7; camera.position.set(-20 - 200 * tmp, 0, -20); camera.rotation.set(0, -Math.PI / 24 - Math.PI * tmp / 2.4, 0);}
+  }
+  else if(x === "Research" && y === "People")
+  {
+    if(z <= 0.1) {let tmp = z; camera.position.set(-45 + 100 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 9 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 0.2) {let tmp = z - 0.1; camera.position.set(-35 + 150 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 18 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 0.3) {let tmp = z - 0.2; camera.rotation.set(-20 + 30 * tmp, 0, 210 - 100 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.4) {let tmp = z - 0.3; camera.position.set(-17, 0, 200 - 2200 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.8) {let tmp = z - 0.4; camera.position.set(-17 - 7.5 * tmp, - 10 * tmp, -20 - 20 * tmp); camera.rotation.set(0, - Math.PI * tmp / 1.2, 0);}
+    else if(z <= 1) {let tmp = (z - 0.8) / 2; camera.position.set(-20, -4 - 10 * tmp, -28 - 20 * tmp); camera.rotation.set(0, -Math.PI * 4 / 12 - Math.PI * tmp / 1.2, 0);}
+  }
+  else if(x === "People" && y === "Research")
+  {
+    if(z <= 0.2) {let tmp = z / 2; camera.position.set(-20, -5 + 10 * tmp, - 30 + 20 * tmp); camera.rotation.set(0, -Math.PI * 5 / 12 + Math.PI * tmp / 1.2, 0);}
+    else if(z <= 0.6) {let tmp = z - 0.2; camera.position.set(-20 + 7.5 * tmp, -4 + 10 * tmp, -28 + 20 * tmp); camera.rotation.set(0, -Math.PI * 4 / 12 + Math.PI * tmp / 1.2, 0);}
+    else if(z <= 0.7) {let tmp = z - 0.6; camera.position.set(-17, 0, -20 + 2200 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.8) {let tmp = z - 0.7; camera.rotation.set(-17 - 30 * tmp, 0, 200 + 100 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.9) {let tmp = z - 0.8; camera.position.set(-20 - 150 * tmp, 0, 210); camera.rotation.set(0, + Math.PI * tmp / 1.8, 0);}
+    else if(z <= 1) {let tmp = z - 0.9; camera.position.set(-35 - 100 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 18 + Math.PI * tmp / 1.8, 0);}
+  }
+  else if(x === "People" && y === "Join Us")
+  {
+    if(z <= 0.4) {let tmp = z / 4; camera.position.set(-20 + 30 * tmp, -5 + 50 * tmp, -30 + 100 * tmp); camera.rotation.set(0, -Math.PI * 5 / 12 + Math.PI * 5 * tmp / 1.2, 0);}
+    else if(z <= 0.7) {let tmp = (z - 0.4) / 3; camera.position.set(-17, 0, -20 + 1000 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-17 - 230 * tmp, -30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 * tmp / 2.4, 0);}
+  }
+  else if(x === "Join Us" && y === "People")
+  {
+    if(z <= 0.3) {let tmp = z / 3; camera.position.set(-40 + 230 * tmp, -3 + 30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 / 24 - Math.PI * 5 * tmp / 2.4, 0);}
+    else if(z <= 0.6) {let  tmp =(z - 0.3) / 3; camera.position.set(-17, 0, 80 - 1000 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.6)/4; camera.position.set(-17 - 30 * tmp, -20 - 100 * tmp); camera.rotation.set(0, -Math.PI * 5 * tmp / 1.2, 0);}
+  }
+  else if(x === "Join Us" && y === "News")
+  {
+    let tmp = z / 10; camera.position.set(-40 + 100 * tmp, -3 + 30 * tmp, 80 + 200 * tmp);  camera.rotation.set(0, Math.PI * 5 / 24 - Math.PI * 5 * tmp/ 2.4, 0);
+  }
+  else if(x === "News" && y === "Join Us")
+  {
+    let tmp = z / 10; camera.position.set(-30 - 100 * tmp,  -30 * tmp, 100 - 200 * tmp);  camera.rotation.set(0, Math.PI * 5 * tmp / 2.4, 0);
+  }
+  else if(x === "News" && y === "Research")
+  {
+    if(z <= 0.4) {let tmp = z / 4; camera.position.set(-30 + 130 * tmp, 0, 100); camera.rotation.set(0, 0, 0);}
+    else if(z <= 0.7) {let tmp = (z - 0.4) / 3; camera.position.set(-17, 0, 100 + 1100 * tmp); camera.rotation.set(0, Math.PI * tmp / 1.8, 0);}
+    else if(z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-17 - 280 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 18 + Math.PI * tmp / 1.8, 0);}
+  }
+  else if(x === "Research" && y === "News")
+  {
+    if(z <= 0.3) {let tmp = z / 3; camera.position.set(-45 + 280 * tmp, 0, 210); camera.rotation.set(0, Math.PI / 9 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 0.6) {let tmp = (z - 0.3) / 3; camera.position.set(-17, 0, 210 - 1100 * tmp); camera.rotation.set(0, Math.PI / 18 - Math.PI * tmp / 1.8, 0);}
+    else if(z <= 1) {let tmp = (z - 0.6) / 4; camera.position.set(-17 - 130 * tmp, 0, 100); camera.rotation.set(0, 0, 0);}
+  }
+  else if(x === "About" && y === "People")
+  {
+    let tmp = z / 10; camera.position.set(-80 + 600 * tmp,  -50 * tmp, - 300 * tmp);  camera.rotation.set(0, -Math.PI / 6 - Math.PI * tmp / 0.4, 0);
+  }
+  else if(x === "People" && y === "About")
+  {
+    let tmp = z / 10; camera.position.set(-20 - 600 * tmp,  -5 + 50 * tmp, -30 + 300 * tmp);  camera.rotation.set(0, -Math.PI / 6 - Math.PI * tmp / 0.4, 0);
+  }
+  else if(x === "Research" && y === "Join Us")
+  {
+    if(z <= 0.4) {let tmp = z / 4; camera.position.set(-45 + 280 * tmp, 0, 210); camera.rotation.set(0, Math.PI * 1 / 9 - Math.PI * tmp / 0.9, 0);}
+    else if(z <= 0.7) {let tmp = (z - 0.4) / 3; camera.position.set(-17, 0, 210 - 1300 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-17 - 230 * tmp, -30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 * tmp / 2.4, 0);}
+  }
+  else if(x === "Join Us" && y === "Research")
+  {
+    if(z <= 0.3) {let tmp = z / 3; camera.position.set(-40 + 230 * tmp, -3 + 30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 / 24 - Math.PI * 5 * tmp / 2.4, 0);}
+    else if(z <= 0.6) {let tmp = (z - 0.3) / 3; camera.position.set(-17, 0, 80 + 1300 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.6) / 4; camera.position.set(-17 - 280 * tmp, 0, 210); camera.rotation.set(0, + Math.PI * tmp / 0.9, 0);}
+  }
+  else if(x === "People" && y === "News")
+  {
+    if(z <= 0.4) {let tmp = z / 4; camera.position.set(-20 + 30 * tmp, -5 + 50 * tmp, -30); camera.rotation.set(0, -Math.PI * 5 / 12 + Math.PI * 4 * tmp / 1.2, 0);}
+    if(z <= 0.7) {let tmp = (z - 0.4) / 3; camera.position.set(-17, 0, -30 + 1300 * tmp); camera.rotation.set(0, -Math.PI * 1 / 12, 0);}
+    if(z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-17 - 130 * tmp, -0, 100); camera.rotation.set(0, -Math * 1 /12 + Math.PI * tmp / 1.2, 0);}
+  }
+  else if(x === "News" && y === "People")
+  {
+    if(z <= 0.3) {let tmp = z / 3;  camera.position.set(-30 + 130 * tmp, 0, 100); camera.rotation.set(0, Math.PI * tmp / 1.2, 0);}
+    else if(z <= 0.6) {let tmp = (z - 0.3) / 3; camera.position.set(-17, 0, 100 - 1300 * tmp); camera.rotation.set(0, -Math.PI * 1 / 12, 0);}
+    else if(z <= 1) {let tmp = (z -0.6) / 4; camera.position.set(-17 - 30 * tmp, -50 * tmp, -30); camera.rotation.set(0, -Math.PI * 1 / 12 - Math.PI * 4 * tmp / 1.2, 0);}
+  }
+  else if(x === "Join Us" && y === "About")
+  {
+    if(z <= 0.4) {let tmp = z / 4; camera.position.set(-40 + 230 * tmp, -3 + 30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 / 24 - Math.PI * 5 * tmp / 2.4, 0);}
+    else if(z <= 0.7) {let tmp = (z - 0.4) / 3; camera.position.set(-17, 0, 80 - 1000 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.7) / 3; camera.position.set(-17 - 630 * tmp, 0, -20); camera.rotation.set(0, -Math.PI * tmp / 0.6, 0);}
+  }
+  else if(x === "About" && y === "Join Us")
+  {
+    if(z <= 0.3) {let tmp = z / 3; camera.position.set(-80 + 650 * tmp, 0, -20); camera.rotation.set(0, -Math.PI / 6 + Math.PI * tmp / 0.6, 0);}
+    else if(z <= 0.6) {let tmp = (z - 0.3) / 3; camera.position.set(-17, 0, -20 + 1000 * tmp); camera.rotation.set(0, 0, 0);}
+    else if(z <= 1) {let tmp = (z - 0.6) / 4; camera.position.set(-17 - 230 * tmp, -30 * tmp, 80); camera.rotation.set(0, Math.PI * 5 * tmp/ 2.4, 0);}
+  }
+}
 
 const useStyles = makeStyles(theme => ({
   appbar: { position: 'sticky', top: 0 },
