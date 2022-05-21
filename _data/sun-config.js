20a768ace53d0830cd84487a39d5e52ab825ed4f
hpf9017 <hpf9017@gmail.com>
2022-05-21 16:50:09 +0800

    vision team2group && people order

--- _data/sun-config.js
+++ _data/sun-config.js
@@ -34,8 +34,8 @@ export const MEMBERS = [
     { id: 0, name: "Zhou, Guyue", role: "Group Leader", photo: "/member_photos/people_Zhou_Guyue.jpeg" },
     { id: 1, name: "Shi, Yongliang", role: "Postdoc Fellow", photo: "/member_photos/people_Shi_Yongliang.jpeg" },
     { id: 2, name: "Zhou, Qiang", role: "Researcher", photo: "/member_photos/people_Zhou_Qiang.jpg" },
-    { id: 3, name: "Chen, Xiaoxue", role: "PhD Student", photo: "/member_photos/people_Chen_Xiaoxue.jpg" },
-    { id: 4, name: "Li, Pengfei", role: "PhD Student", photo: "/member_photos/people_Li_Pengfei.jpeg" },
-    { id: 5, name: "Li, Yang", role: "Researcher", photo: "/member_photos/people_Li_Yang.jpg" },
-    { id: 6, name: "Tian, Beiwen", role: "Researcher", photo: "/member_photos/people_Tian_Beiwen.jpg" },
+    { id: 3, name: "Li, Yang", role: "Researcher", photo: "/member_photos/people_Li_Yang.jpg" },
+    { id: 4, name: "Tian, Beiwen", role: "Researcher", photo: "/member_photos/people_Tian_Beiwen.jpg" },
+    { id: 5, name: "Chen, Xiaoxue", role: "PhD Student", photo: "/member_photos/people_Chen_Xiaoxue.jpg" },
+    { id: 6, name: "Li, Pengfei", role: "PhD Student", photo: "/member_photos/people_Li_Pengfei.jpeg" },
 ]
