ebdf1bfa1b54a1dfcf8a933606eb1a937fa0a358
TB5zhh <tianbeiwen88@gmail.com>
2022-05-29 20:51:54 +0800

    update the model in homepage

--- src/legacy.js
+++ src/legacy.js
@@ -14,7 +14,7 @@ const Model = ({ onLoading }) => {
     const ref = useRef()
     const scenes = useLoader(GLTFLoader,
         [
-            "/three/new3.glb",
+            "/three/new5.glb",
             // "/three/office1.glb",
             // "/three/office2.glb",
             // "/three/indoor.glb",
