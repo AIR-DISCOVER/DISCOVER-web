c6a2a0dfda31dff034a1b2e3842a7cbb3ef3afa6
TB5zhh <tianbeiwen88@gmail.com>
2023-02-19 01:44:19 +0800

    update robotics

--- _data/discover-config.js
+++ _data/discover-config.js
@@ -1,5 +1,5 @@
 export const PHONE_NUMBER = '(010) 8215 1160'
 
-export const EMAIL = 'zhouguyue@air.tsinghua.edu.cn'
+export const EMAIL = 'zhouguyue@air.tsinghua.edu.cn (test)'
 
 export const ADDRESS = 'Addr: 12 / F, block C, Qidi science and technology building, Tsinghua Science and Technology Park, Beijing'
