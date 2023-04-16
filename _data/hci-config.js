22cbd0550c26532b6fca1de330ed3851a512a429
TB5zhh <tianbeiwen88@gmail.com>
2023-04-16 14:14:28 +0800

    update hci configs

--- _data/hci-config.js
+++ _data/hci-config.js
@@ -63,9 +63,9 @@ export const CATEGORIES = [
 
 export const MEMBERS = [
     { id: 1, name: "Gong, Jiangtao", role: "Group Leader", photo: "/member_photos/people_Gong_Jiangtao.jpg", url: "https://air.tsinghua.edu.cn/info/1046/1635.htm" },
-    { id: 2, name: "Gao, Jiasi", role: "Postdoc Fellow", photo: "/member_photos/people_Gao_Jiasi.jpg" },
-    { id: 3, name: "Chu, Mengdi", role: "Researcher", photo: "/member_photos/people_Chu_Mengdi.jpg" },
-    { id: 4, name: "Zhang, Yan", role: "Researcher", photo: "/member_photos/people_Zhang_Yan.jpg" },
-    { id: 5, name: "Zheng, Chen", role: "Postdoc Fellow", photo: "/member_photos/people_Zheng_Chen.jpg" },
+    { id: 2, name: "Zheng, Chen", role: "Postdoc Fellow", photo: "/member_photos/people_Zheng_Chen.jpg" },
+    { id: 3, name: "Gao, Jiasi", role: "Postdoc Fellow", photo: "/member_photos/people_Gao_Jiasi.jpg" },
+    { id: 4, name: "Chu, Mengdi", role: "Researcher", photo: "/member_photos/people_Chu_Mengdi.jpg" },
+    { id: 5, name: "Zhang, Yan", role: "Researcher", photo: "/member_photos/people_Zhang_Yan.jpg" },
     
 ]
